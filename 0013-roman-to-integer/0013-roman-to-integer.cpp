fa18ebd0b14a4e0a31a9c31ebc2d08e9dcdbc1e4
Nidhish <168630210+nidhishkk@users.noreply.github.com>
2026-08-11 20:12:33 +0530

    Time: 14 ms (11.85%), Space: 13.5 MB (18.28%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -3,13 +3,15 @@ public:
     int romanToInt(string s) {
         unordered_map<char,int> equi={{'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000}};
         int n=s.size(),num=0;
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n-1;i++){
             if(equi[s[i]]<equi[s[i+1]]){
                 num-=equi[s[i]];
             }
             else{
                 num+=equi[s[i]];
             }
-        }return num;
+        }
+        num+=equi[s[n-1]];
+        return num;
     }
 };
